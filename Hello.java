5bcf734f81c70ae539a4f4161f8bc539190e230e
NguyenLinh2000 <18073011.linh@student.iuh.edu.vn>
2021-09-15 17:31:06 +0700

    demo commit to code by cmd

--- Hello.java
+++ Hello.java
@@ -1,7 +1,11 @@
 import java.util.Scanner;
 
+import jdk.javadoc.internal.doclets.formats.html.SourceToHTMLConverter;
+
 public class Hello {
     public static void main(String[] args) {
+
+		System.out.println("Hello Week 2");
 
 		Scanner scanner = new Scanner(System.in);
 
